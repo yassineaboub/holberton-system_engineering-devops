b3673ce291ed6b5a268f605e2e286e9a8963a4b0
Mohamed Yassine <yassineaboub@live.fr>
2020-05-04 05:08:40 +0000

    first

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import requests
+import sys
+if __name__ == "__main__":
+    id = sys.argv[1]
+    x = 0
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    user = requests.get(url).json()
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                          format(id)).json()
+    tasks = []
+    for n in todos:
+        if n.get('completed') == True:
+            tasks.append(n['title'])
+            x += 1
+    print("Employee {} is done with tasks({}/{}):".format(user.get('name'), x, len(n)))
+
+    for n in tasks:
+        print("\t {}".format(n))

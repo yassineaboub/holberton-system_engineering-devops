5faaedc913b56c875d0068b0875f65282fbdd948
Mohamed Yassine <yassineaboub@live.fr>
2020-05-04 05:28:57 +0000

    1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import requests
+import sys
+if __name__ == "__main__":
+    id = sys.argv[1]
+    x = 0
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    user = requests.get(url).json()
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                         .format(id)).json()
+    with open("{}.csv".format(id), 'w') as CVSfile:
+        writer = csv.writer(CVSfile, delimiter=',', quoting=QUOTE_ALL)
+        for line in todos:
+            writer.writerow([id, user.get('username'),
+                           task.get('completed'), task.get('title')])
+

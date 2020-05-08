bed466423ca53bfa62811e3b486e261c406a5e96
Mohamed Yassine <yassineaboub@live.fr>
2020-05-08 07:34:04 +0000

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress"""
 import requests
 import sys
 if __name__ == "__main__":

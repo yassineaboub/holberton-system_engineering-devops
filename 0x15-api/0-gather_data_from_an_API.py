4e3ddfe9266cf0521d56bbb4b59e84b4207a60cf
Mohamed Yassine <yassineaboub@live.fr>
2020-05-08 07:27:10 +0000

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,17 +4,16 @@ import sys
 if __name__ == "__main__":
     id = sys.argv[1]
     x = 0
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
-    user = requests.get(url).json()
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(id)).json()
     todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
                          .format(id)).json()
     tasks = []
     for n in todos:
         if n.get('completed') is True:
-            tasks.append(n['title'])
+            tasks.append(n.get('title'))
             x += 1
     print("Employee {} is done with tasks({}/{}):".
-          format(user.get('name'), x, len(n)))
+          format(user.get('name'), x, len(todos)))
 
     for n in tasks:
         print("\t {}".format(n))

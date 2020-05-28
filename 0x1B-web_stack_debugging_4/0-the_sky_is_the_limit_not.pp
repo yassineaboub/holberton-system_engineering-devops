4a40b19faa9e16e0e35164ad838984af908da690
root <yassineaboub@live.fr>
2020-05-28 23:10:09 +0000

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Fix Failed requests
 exec { 'change_requests_limit':
-  command  => "sed -i 's/worker_processes 4;/worker_processes 100;/g' /etc/nginx/nginx.conf && sudo service nginx restart",
+  command => "sed -i 's/worker_processes 4;/worker_processes 100;/g' /etc/nginx/nginx.conf && sudo service nginx restart",
   path    => ['/usr/bin', '/usr/sbin', '/bin'],
 }

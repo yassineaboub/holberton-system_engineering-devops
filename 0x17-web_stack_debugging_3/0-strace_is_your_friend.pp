33f83c2dad40afcc52bf7e11761ecb5e0c4c00b1
yassineaboub <yassineaboub@live.fr>
2020-06-21 19:42:19 +0000

    fix using strace

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+#Replace phpp by php
+exec { 'fix using strace':
+  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g'
+              /var/www/html/wp-settings.php",
+  path    => ['/bin'],
+}
